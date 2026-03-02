589d017b7ecf295b2efa5febe006b386d902b358
Kalvin Melo <kalvin.melo.km@gmail.com>
2026-03-02 08:53:08 -0300

    :sparkles: feat(ui): add custom display font and integrate into theme

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         padding: '20px',
       }}
     >
-      <h1>Dev Flash Cards</h1>
+      <h1 className="app-title">Dev Flash Cards</h1>
 
       <AreasSection />
     </div>
